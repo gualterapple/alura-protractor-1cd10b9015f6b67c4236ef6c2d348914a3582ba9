08a6bfbc2c533e6b90c503b012f3e295d1c3e59a
gualterapple <39972000+gualterapple@users.noreply.github.com>
2022-03-21 09:15:05 +0100

    Testando filtro

--- e2e/src/home.e2e-spec.ts
+++ e2e/src/home.e2e-spec.ts
@@ -1,26 +1,36 @@
 import { browser, element, by, protractor } from 'protractor';
 
-describe('', () => {
+describe('Home Page', () => {
+
+  beforeEach( async () => {
+    await browser.get(`${browser.baseUrl}/#/user/flavio`);
+  });
+
 
   it('Should navigate to user profile', async () => {
-    await browser.get(`${browser.baseUrl}/#/user/flavio`);
     const title = await browser.getTitle();
     expect(title).toEqual('Timeline');
   });
 
   it('Should display a list of photos', async () => {
-    await browser.get(`${browser.baseUrl}/#/user/flavio`);
     const list = element.all(by.css('.photo'));
     const photoListSize = await list.count();
-    expect(photoListSize).toBeGreaterThan(0)
+    expect(photoListSize).toBeGreaterThan(0);
    });
 
    it('Should navigate to photo detail when photo navigation is triggered', async () => {
-    await browser.get(`${browser.baseUrl}/#/user/flavio`);
     const firstElement = element.all(by.css('.photo')).first();
     await firstElement.sendKeys(protractor.Key.ENTER);
     const title = await browser.getTitle();
     expect(title).toBe('Photo detail');
+   });
+
+   it('Should list one item when filtering by word "farol"', async () => {
+    const searchInput = element(by.css('ap-search input[type=search]'));
+    await searchInput.sendKeys('farol');
+    const list = element.all(by.css('.photo'));
+    const photoListSize = await list.count();
+    expect(photoListSize).toBe(1);
    });
 
 });
