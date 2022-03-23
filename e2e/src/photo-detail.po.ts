f9ff85ed33c4aaefd7d4a61b2342d608ed1f6750
gualterapple <39972000+gualterapple@users.noreply.github.com>
2022-03-23 01:47:19 +0000

    Preparando photo-detail.po para testar os comentarios nas fotos

--- e2e/src/photo-detail.po.ts
+++ e2e/src/photo-detail.po.ts
@@ -1,4 +1,4 @@
-import { browser } from "protractor";
+import { browser, by, element } from "protractor";
 
 export class PhotoDetailPage
 {
@@ -8,5 +8,18 @@ export class PhotoDetailPage
   getWindowTitle()
   {
     return browser.getTitle();
+  }
+
+  fillComment(text: string){
+    return element(by.css('')).sendKeys(text);
+  }
+
+  publishComment() {
+    return element(by.css('button[type=submit]')).click();
+  }
+
+  getCommentListSize(){
+    return element.all(by.css('ap-photo-comments li'))
+    .count();
   }
 }
